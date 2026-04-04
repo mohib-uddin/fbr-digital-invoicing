fa4c50eb52125c7e710de0cc6c840b2ca1664b2b
Salman <salman.sa337@gmail.com>
2026-04-04 16:23:01 +0500

    fix: server start issues

--- backend/src/customer/customer.module.ts
+++ backend/src/customer/customer.module.ts
@@ -3,9 +3,10 @@ import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from './entities/customer.entity';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer])],
+  imports: [TypeOrmModule.forFeature([Customer, User])],
   controllers: [CustomerController],
   providers: [CustomerService],
   exports: [CustomerService],
